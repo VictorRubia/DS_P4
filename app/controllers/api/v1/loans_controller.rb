a5b10d9d1e75b7e9f67fc87c45d0a5bb3677a68d
VictorRubia <victorrubia@correo.ugr.es>
2021-05-25 12:07:25 +0200

    Añado api loans

--- app/controllers/api/v1/loans_controller.rb
+++ app/controllers/api/v1/loans_controller.rb
@@ -1,1 +1,56 @@
 
+module Api
+  module V1
+    class AccountsController < ApplicationController
+
+      skip_before_action :verify_authenticity_token
+
+      def show
+        @loan = Loan.find_by(id: params[:id])
+        if (@loan!=nil)
+          render json: @loan, status: :ok, :include => [:cards, :loans, :requests, :transfers, :investments, :transactions]
+        else
+          render json: :nothing, status: :not_found
+        end
+      end
+
+      def index
+        render json: Loan.all, status: :ok, :include => [:cards, :loans, :requests, :transfers, :investments, :transactions]
+      end
+
+      def update
+        @loan = Loan.find(params[:id])
+
+        if @loan.update(account_params)
+          render json: @loan, status: :ok
+        else
+          render json: @loan.errors, status: :unprocessable_entity
+        end
+      end
+
+      def create
+        @loan = Loan.new(account_params)
+
+        if @loan.save
+          render json: @loan, status: :created
+        else
+          render json: @loan.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @loan = Loan.find(params[:id])
+        if @loan.destroy
+          render json: :nothing, status: :ok
+        else
+          render json: :nothing, status: :unprocessable_entity
+        end
+      end
+      private
+      def account_params
+        params.require(:loan).permit(:amount, :meses, :interes, :account_id)
+      end
+    end
+  end
+end
+
