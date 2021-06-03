43174e2aae9d096d5a408ee4f3158d1677bec27c
VictorRubia <victorrubia@correo.ugr.es>
2021-06-03 22:50:07 +0200

    Probar si esta jquery

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,3 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min   // Add it after jquery and jquery_ujs
--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,19 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: %i[ show edit update destroy ]
 
+  # POST /ajax/sum
+  def aceptar_prestamo
+    puts "HOLAAA"
+    num1 = params["num1"].to_i
+    num2 = params["num2"].to_i
+    # Do something with input parameter and respond as JSON with the output
+    result = num1 + num2
+
+    respond_to do |format|
+      format.json {render :json => {:result => result}}
+    end
+  end
+
   # GET /accounts or /accounts.json
   def index
     @accounts = Account.all
